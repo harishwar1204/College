76c50eda1ee78378504645bf2b7bcef3a89c3669
Harishwar <kasireddy.rithvik@gmail.com>
2022-10-27 23:22:36 -0400

    first commit

---
+++ SQL/Student_table.sql
@@ -0,0 +1,16 @@
+create table student (
+std_id int,
+std_fst_nm varchar(20),
+std_lst_nm varchar(20),
+dept_nm varchar(10),
+dept_id int,
+std_email_id varchar(50),
+std_address varchar(100),
+std_phn int(10),
+std_dob varchar(10)
+);
+select * from student;
+
+
+
+
